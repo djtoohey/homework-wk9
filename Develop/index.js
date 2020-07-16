83327c5a97a71c7e7a4619d7737363c44ce74e89
djtoohey <djtoohey@gmail.com>
2020-07-16 16:11:11 +0930

    everything pushes to screen, need to add more license info, then should be good to go

--- Develop/index.js
+++ Develop/index.js
@@ -80,10 +80,60 @@ function writeToFile(fileName, data) {
 function init() {
     // console.log(genMD.generateMarkdown = "test");
     inquirer.prompt(questions).then(function (data = { title, description, install, usage, license, contributing, tests, ghUsername, email, fullname, year }) {
+        let license = ""
+        if (data.license === "MIT") {
+            license = `MIT License
 
+            Copyright (c)  ${data.year} ${data.fullname}
+            
+            Permission is hereby granted, free of charge, to any person obtaining a copy
+            of this software and associated documentation files (the "Software"), to deal
+            in the Software without restriction, including without limitation the rights
+            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+            copies of the Software, and to permit persons to whom the Software is
+            furnished to do so, subject to the following conditions:
+            
+            The above copyright notice and this permission notice shall be included in all
+            copies or substantial portions of the Software.
+            
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            SOFTWARE.`
+        }
+        else if (data.license === "GNU General Public License v3.0)") {
+            license = `GNU LICENSE`
+
+        }
 
         const readme =
             `# ${data.title}
+            
+
+${data.description}
+
+## Installation:
+    ${data.install}
+
+## Usage
+${data.usage}
+
+## License
+${license}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+GitHub: https://www.github.com/${data.ghUsername}/
+
+Email: ${data.email}
         `
         writeToFile("test.md", readme);
 
