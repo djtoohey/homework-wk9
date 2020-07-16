071b8f2c5f8fde46160aea1b5f2b09e9bf083013
djtoohey <djtoohey@gmail.com>
2020-07-16 16:58:45 +0930

    added generate license, still tossing up how to change better

--- Develop/generateLicense.js
+++ Develop/generateLicense.js
@@ -1,16 +1,32 @@
-# tst
-            
-
-test
-
-## Installation:
-    
-
-## Usage
 
 
-## License
-GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+
+// function to generate License for README
+function generateLicense(data) {
+
+    const MIT = `MIT License
+
+Copyright (c) ${data.year} ${data.name}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`;
+
+    const GNU = `GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 
     Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
     Everyone is permitted to copy and distribute verbatim copies
@@ -627,16 +643,44 @@ GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
     reviewing courts shall apply local law that most closely approximates
     an absolute waiver of all civil liability in connection with the
     Program, unless a warranty or assumption of liability accompanies a
-    copy of the Program in return for a fee.
+    copy of the Program in return for a fee.`
 
-## Contributing
+    const Unlicense = `This is free and unencumbered software released into the public domain.
 
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
 
-## Tests
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
 
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
 
-## Questions
-GitHub: https://www.github.com//
+For more information, please refer to <https://unlicense.org>`;
 
-Email: 
-        
+    let licenseExport;
+    if (data.license === "MIT") {
+        licenseExport = MIT;
+    }
+    else if (data.license === "GNU General Public License v3.0") {
+        licenseExport = GNU;
+    }
+    else {
+        licenseExport = Unlicense;
+    }
+    return licenseExport;
+}
+
+module.exports = generateLicense;
